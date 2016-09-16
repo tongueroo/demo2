96785f0308afb68a4dd688b95067798f18e91d83
Lucy <gr-lucy@gmail.com>
2016-09-15 17:23:21 -0700

    init commit

---
+++ hotdog.rb
@@ -0,0 +1,25 @@
+class Hotdog
+  def make
+    puts "making hotdog"
+  end
+
+  def eat
+    puts "eating hotdog"
+  end
+
+  def apply_ketchup
+    puts "applying ketchup"
+  end
+
+  def apply_mustard
+    puts "applying mustard"
+  end
+
+  def apply_relish
+    puts "applying relish"
+  end
+
+  def toss
+    puts "tossing hotdog"
+  end
+end
