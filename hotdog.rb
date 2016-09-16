1bfca5868dc416a99ececa2274a2e4b68c12db58
Chuck <gr-chuck@gmail.com>
2016-09-15 17:23:22 -0700

    remove mustard

--- hotdog.rb
+++ hotdog.rb
@@ -6,12 +6,4 @@ class Hotdog
   def apply_ketchup
     puts "applying ketchup"
   end
-
-  def apply_mustard
-    puts "applying mustard"
-  end
-
-  def apply_relish
-    puts "applying relish"
-  end
 end
